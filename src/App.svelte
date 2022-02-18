3bfaf7f939a2f4b92b2f780f118009fd449df748
Ryan Miller <ryan8573@gmail.com>
2022-02-18 01:13:02 -0500

    learning

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
 	export let name;
+	const pkg = {
+		name: "Ryan",
+		version: "0.1",
+	};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Ryan's {name}</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<button> Help </button>
 </main>
 
 <style>
+	:global(body) {
+		background-color: black;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -19,7 +30,7 @@
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: 200;
 	}
 
 	@media (min-width: 640px) {
